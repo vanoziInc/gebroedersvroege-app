491449d03632611358c2b5c8d04fd8cff6a45562
vanozi <wouter.vanos86@gmail.com>
2022-01-23 13:02:45 +0100

    concrete test

--- frontend-testing/test_exampe.py
+++ frontend-testing/test_exampe.py
@@ -1,5 +1,4 @@
   
 def test_example_is_working(page):
-    page.goto("https://example.com")
-    assert page.inner_text('h1') == 'Example Domain'
-    page.click("text=More information")
+    page.goto("http://frontend.localhost/auth/login")
+    assert page.inner_text('//input[@data-test="email-input"]/preceding-sibling::label') == 'E-mail adres'
