cead67ab1ad3c1d0595c2cb5919934c0fb4b94d6
vanozi <wouter.vanos86@gmail.com>
2022-01-23 22:08:18 +0100

    nu echt ?

--- frontend-testing/test_example.py
+++ frontend-testing/test_example.py
@@ -1,6 +1,5 @@
 import pytest
 
-@pytest.mark.uitests
 def test_example_is_working(page):
     page.goto("http://frontend.localhost/auth/login")
     assert page.inner_text('//input[@data-test="email-input"]/preceding-sibling::label') == 'E-mail adres'
