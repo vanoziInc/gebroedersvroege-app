c5681b8a2d4cc56643737c64eb4d29d427bc8c33
vanozi <wouter.vanos86@gmail.com>
2022-01-23 22:00:29 +0100

    nog maar eens kieken

--- frontend-testing/conftest.py
+++ frontend-testing/conftest.py
@@ -1,7 +1,7 @@
-import pytest
+# import pytest
 
 
 
-@pytest.fixture(scope="session")
-def browser_type_launch_args(browser_type_launch_args):
-    return {"proxy" : { "server" : "host.docker.internal:80", "bypass":"loopback" }}
+# @pytest.fixture(scope="session")
+# def browser_type_launch_args(browser_type_launch_args):
+#     return {"proxy" : { "server" : "host.docker.internal:80", "bypass":"loopback" }}
